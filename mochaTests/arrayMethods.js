91494807dae8a5c21e012402247ff8ed8205db72
Keith Levi <guilddrive@gmail.com>
2021-03-05 13:39:49 -0600

    noon work

---
+++ mochaTests/arrayMethods.js
@@ -0,0 +1,18 @@
+"use strict";
+
+exports.insertArray = insertArray;
+//exports.getMiddle = getMiddle;
+
+
+/* 0.  Write a function that will take an index and two arrays and splice the second array into the first at the given position */
+
+/**
+ * 
+ * @param {number} index of place to insert
+ * @param {Array} source the array to be spliced
+ * @param {Array} insert the array to be inserted
+ * @returns {Array} the resulting array
+ */
+function insertArray(index, source, insert){
+//your code here
+}
