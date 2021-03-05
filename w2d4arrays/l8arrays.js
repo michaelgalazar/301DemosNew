329fd1114c495c87c02ad0c692a7f7696323052d
KeithLevi <guilddrive@gmail.com>
2021-03-05 15:37:01 -0600

    Update l8arrays.js

--- w2d4arrays/l8arrays.js
+++ w2d4arrays/l8arrays.js
@@ -10,12 +10,17 @@ matrix addition of [[0,1,2],[9,8,7]] and [[6,5,4], [3,4,5]]  should be [ [ 6, 6,
 
 /**
  * 
- * @param {Array} matrix1 is 2d array
- * @param {Array} matrix2 is 2d array
- * @returns {Array} sum array
+ * @param {Array} matrix1 is 2d array of same sized arrays as elements
+ * @param {Array} matrix2 is 2d array of exact same dimensions as matrix1
+ * @returns {Array} sum of input arrays
  */
 function matrixAddition(matrix1, matrix2) {
-    let sumMatrix = [[],[]];
+    let sumMatrix = [];
+
+    /* initialize the sumMatrix to be the same size as the adding matrices */
+    for (let i = 0; i < matrix1.length; i++) {
+        sumMatrix.push([]);
+    }
 
     for (let i = 0; i < matrix1.length; i++) {  //go through outside array
         for (let j = 0; j < matrix1[i].length; j++) { //go through each of the inner arrays in turn
